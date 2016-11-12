577604095d410d848d87ca8a7e8636dfe74e2ef7
Brittney Chin <brittney.chin@mymona.uwi.edu>
2016-11-12 03:17:56 +0000

    XML displays in console

--- request.php
+++ request.php
@@ -4,6 +4,7 @@
 // respond with a value
 
 $query = $_GET['q'];
+$all = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -14,5 +15,19 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmlData= '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name="definition" author="John">a statement of the exact meaning of a word, especially in a dictionary.</definition>
+        <definition name="bar" author="mary">a place that sells alcholic beverages</definition>
+        <definition name="ajax" author="Kimberly">technique which involves the use of javascript and xml</definition>
+    </entries>';
+    
+if($all){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmlData);
+    echo $xmlOutput->asXML();
+}
+else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
